30c400380f4a6db3640e990ede25754fd2ccaf11
Anthony Muli <mulianthony561@gmail.com>
2020-01-28 21:34:28 +0300

    added an error file to load on the page with a certain message from the rendered template when the appropriate data is not found

--- app/error.py
+++ app/error.py
@@ -1,1 +1,10 @@
 # Where we will create handlers for error pages.
+from flask import render_template
+from app import app
+
+@app.errorhandler(404)
+def four_Ow_four(error):
+    '''
+    Function to render the 404 error page
+    '''
+    return render_template('fourOwfour.html'),404
