4d9cf105176d3ca32c946f80d579b51a5d616e17
Anthony Muli <mulianthony561@gmail.com>
2020-01-28 22:03:35 +0300

    added all files created in the activated virtual environment

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from .config import DevConfig
 from flask_bootstrap import Bootstrap
 
+
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
 
@@ -15,3 +16,4 @@ app.config.from_pyfile('config.py')
 bootstrap = Bootstrap(app)
 
 from app import views
+from app import error
